83af70a9553d542273eee69caf57a5c3b5a7a4eb
Hooman Asadi <Hooman_Asadi92@yahoo.com>
2023-11-02 14:36:13 +0330

    this is where I add ChoiceCard for Computer as well

--- src/components/GameChoices.jsx
+++ src/components/GameChoices.jsx
@@ -1,37 +1,66 @@
+import ChoiceCard from "./ChoiceCard.jsx";
+import "./GameChoices.css"; // Make sure to create a corresponding CSS file for styling
+import paperImage from "../assets/paper.png";
+import rockImage from "../assets/rock.png";
+import scissorsImage from "../assets/scissors.png";
 
-import ChoiceCard from './ChoiceCard.jsx';
-import './GameChoices.css'; // Make sure to create a corresponding CSS file for styling
-import paperImage from '../assets/paper.png'; 
-import rockImage from '../assets/rock.png'; 
-import scissorsImage from '../assets/scissors.png'; 
 const GameChoices = ({ playerName, playerCity }) => {
   // Define gameRecords inside the component
   const gameRecords = {
     rock: { wins: 0, losses: 0, ties: 0 },
     paper: { wins: 0, losses: 0, ties: 0 },
-    scissors: { wins: 0, losses: 0, ties: 0 }
+    scissors: { wins: 0, losses: 0, ties: 0 },
+  };
+
+  // Define a separate set of records for the computer
+  const computerRecords = {
+    rock: { wins: 0, losses: 0, ties: 0 },
+    paper: { wins: 0, losses: 0, ties: 0 },
+    scissors: { wins: 0, losses: 0, ties: 0 },
   };
 
   return (
     <div className="game-choices-container">
-      <ChoiceCard 
-        choice={rockImage} 
-        playerName={playerName} 
-        playerCity={playerCity} 
-        record={gameRecords.rock} 
-      />
-      <ChoiceCard 
-        choice= {paperImage} 
-        playerName={playerName} 
-        playerCity={playerCity} 
-        record={gameRecords.paper} 
-      />
-      <ChoiceCard 
-        choice={scissorsImage} 
-        playerName={playerName} 
-        playerCity={playerCity} 
-        record={gameRecords.scissors} 
-      />
+      <div className="player-cards">
+        <ChoiceCard
+          choice={rockImage}
+          playerName={playerName}
+          playerCity={playerCity}
+          record={gameRecords.rock}
+        />
+        <ChoiceCard
+          choice={paperImage}
+          playerName={playerName}
+          playerCity={playerCity}
+          record={gameRecords.paper}
+        />
+        <ChoiceCard
+          choice={scissorsImage}
+          playerName={playerName}
+          playerCity={playerCity}
+          record={gameRecords.scissors}
+        />
+      </div>
+      <div className="computer-cards">
+        <ChoiceCard
+          choice={rockImage}
+          playerName="Computer"
+          playerCity="Tech"
+          record={computerRecords.rock}
+        />
+        <ChoiceCard
+          choice={paperImage}
+          playerName="Computer"
+          playerCity="Tech"
+          record={computerRecords.paper}
+        />
+        <ChoiceCard
+          choice={scissorsImage}
+          playerName="Computer"
+          playerCity="Tech"
+          record={computerRecords.scissors}
+        />
+      </div>
     </div>
   );
 };
